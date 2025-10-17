768d593134aef3065a66c7a602642ae0ab6660f0
NoahjmHolt <noahjmholt@gmail.com>
2025-10-17 17:15:34 -0600

    Clock time
    Added a clock and set the framerate of the game to be 60 FPS

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # setting game to 60 FPS to reduce CPU costs
+    time_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
         for event in pygame.event.get():
@@ -18,6 +22,8 @@ def main():
 
         screen.fill(color=0)
         pygame.display.flip()
+
+        dt = time_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
