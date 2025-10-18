6efbcce1972ff1813820e4def60a3a1779384657
NoahjmHolt <noahjmholt@gmail.com>
2025-10-18 14:41:14 -0600

    Group creation
    strarted experimenting with using groups to create and do things that will need to be done for everything so that it can get handled easier later.

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def main():
     time_clock = pygame.time.Clock()
     dt = 0
 
+    # experimenting with groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     # making the player
     space_ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 7)
 
@@ -25,13 +31,13 @@ def main():
                 return
 
         dt = time_clock.tick(60) / 1000
-        space_ship.update(dt)
+        for ship in updatable:
+            ship.update(dt)
 
         screen.fill(0)
-        space_ship.draw(screen)
+        for ship in drawable:
+            ship.draw(screen)
         pygame.display.flip()
-
-
 
 
 if __name__ == "__main__":
